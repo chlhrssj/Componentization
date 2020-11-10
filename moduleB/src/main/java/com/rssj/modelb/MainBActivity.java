81549fc23c322d7b9d0fe45cd6465b7dbc3c5e7c
rssj <674878857@qq.com>
2020-11-10 18:24:05 +0800

    组件化

---
+++ moduleB/src/main/java/com/rssj/modelb/MainBActivity.java
@@ -0,0 +1,30 @@
+package com.rssj.modelb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.rssj.modelbase.util.AndroidUtils;
+
+public class MainBActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_b);
+
+        execLogin();
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(l -> {
+            AndroidUtils.startAct(MainBActivity.this, "com.rssj.modelb.ModelBActivity");
+        });
+    }
+
+    private void execLogin() {
+        //模拟登录操作
+    }
+}
